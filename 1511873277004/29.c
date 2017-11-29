53228ec86a52aaeb4165780cb53208d873b8af45
疾风三刀流 <post443@163.com>
2017-11-29 22:05:04 +0800

    2017年11月29日22:05:03

---
+++ 1511873277004/29.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+	float apple=.5,pear=.35,melon=2;
+	printf("*****  ON SALE  *****\n");
+	printf("Fruit type      Price\n");
+	printf("Apple           $%4.2f\nPear            $%4.2f\nMelon           $%4.2f",apple,pear,melon);
+}
